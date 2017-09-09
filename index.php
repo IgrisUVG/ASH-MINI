bdd33390cd22a39ef2495424b18084627e567a1c
IgrisUVG <igris@ushivangoga.ru>
2017-09-09 12:41:35 +0300

    ASH MINI ++

--- index.php
+++ index.php
@@ -17,30 +17,21 @@
 </noscript>
 <script type='text/javascript'>
     (function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-            var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })
+        w[l] = w[l] || [];
+        w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+        var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })
     (window, document, 'script', 'dataLayer', 'GTM-M7S4WT');
 </script>
 <!-- End Google Tag Manager -->
 <div class="header">
-    <div class="logo">
-        <!--a href="http://annashvedova.ru/"-->
-            <img src="/images/ASh.svg" alt="Logo">
-        <!--/a-->
-    </div>
-    <div class="name">
-        <a href="http://annashvedova.ru/">
-            <img src="/images/HEAD_RU_LINE_1.svg" alt="Title">
-        </a>
-    </div>
+        <img src="/images/AShFull.svg" alt="Logo">
 </div><!--Конец шапки-->
 <div class="contents">
-    <img alt="Photo" src="/images/IMG_328711.jpg" />
+    <img alt="Photo" src="/images/IMG_328711.jpg"/>
 </div><!--Конец контента-->
 <div class="footer">
     <?php
